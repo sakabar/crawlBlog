37400fa606c7107f50fcf7f7d2385044d804a32a
Takafumi Sakakibara <takafumi.sakakibara.pdx@gmail.com>
2014-11-09 21:11:34 +0900

    Modify : don't use proxy

--- getBlog.sh
+++ getBlog.sh
@@ -6,9 +6,16 @@ function change_proxy(){
   export http_proxy=http://`cat proxy_list.txt | grep -v "^#" | sort -R | head -n1`/
 }
 
+#元々プロキシを使っていたが、遅いのでやめた。
 function my_wget(){
-  change_proxy
-  wget $1 -T5 --tries=1 --inet4-only --no-verbose -O $2
+  if true; then
+    wget $1 -T2 --tries=1 --inet4-only --no-proxy -O $2
+    sleep $[$RANDOM % 3]
+  else
+  #プロキシを使うコード
+    change_proxy
+     wget $1 -T2 --tries=1 --inet4-only -O $2
+  fi
 }
 
 
@@ -20,20 +27,19 @@ for url in `cat $bloglist`; do
 
   #wgetが失敗したら、proxyを切り換えて再試行
   if test $wget_status -ne 0; then
-    if test `wget $url --spider 2>&1 | grep "404 Not Found" | wc -l` -gt 0; then
+    if test `wget $url --spider -T5 --tries=1 --inet4-only 2>&1 | grep "404 Not Found" | wc -l` -gt 0; then
       #そもそもページが存在しないので、プロキシの問題ではない
       rm entries/$output
-    fi
-  else
-    bad_proxy=`echo $http_proxy | sed -e 's|^http://||' -e 's|/$||'`
-    tmp_file=`mktemp`
-    grep -v $bad_proxy proxy_list.txt > $tmp_file
-    mv $tmp_file proxy_list.txt
+      continue
+    else
+      bad_proxy=`echo $http_proxy | sed -e 's|^http://||' -e 's|/$||'`
+      tmp_file=`mktemp`
+      grep -v $bad_proxy proxy_list.txt > $tmp_file
+      mv $tmp_file proxy_list.txt
 
-    my_wget $url entries/$output
+      my_wget $url entries/$output
+    fi
   fi
 done
 
 echo "ブログ記事のダウンロードが終了しました。"
-
-
