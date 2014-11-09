082a468e4588226a06e3a23668c2130815dbd533
Takafumi Sakakibara <takafumi.sakakibara.pdx@gmail.com>
2014-11-09 20:00:08 +0900

    Modify : extractText.sh, getVerbChunk.sh, parseWithCabocha.sh can take argument (base directory)

--- extractText.sh
+++ extractText.sh
@@ -1,11 +1,17 @@
 #!/bin/zsh
 
+dir=$1
+if test -n $dir; then
+  dir=`pwd`
+fi
+
+
 #<br>などを改行に置換したファイルをmid_data内に置く
-for f in entries/*.html; do
-  sed -e 's|<br />|\n|g' -e 's|<br>|\n|g' $f > mid_data/$f:t
+for f in $dir/entries/*.html; do
+  sed -e 's|<br />|\n|g' -e 's|<br>|\n|g' $f > $dir/mid_data/$f:t
 done
 
-for f in mid_data/*.html; do
-  ruby extract_text.rb $f | nkf -Lu | tr -d '\t' | sed -e 's/ *//g' | grep -v "^$" > text/$f:t:r".txt"
+for f in $dir/mid_data/*.html; do
+  ruby extract_text.rb $f | nkf -Lu | tr -d '\t' | sed -e 's/ *//g' | grep -v "^$" > $dir/text/$f:t:r".txt"
   #rm -f $f
 done
--- getVerbChunk.sh
+++ getVerbChunk.sh
@@ -1,8 +1,13 @@
 #!/bin/zsh
 
-xml_file=$1
-# xml_file="tmp.cabocha.xml"
+dir=$1
+if test -n $dir; then
+  dir=`pwd`
+fi
 
-for f in cabocha_xml/*; do
-  ruby getVerbChunk.rb $f | awk -F, '{print $7}' > verb_chain/$f:t:r".txt"
+echo $dir
+exit
+
+for f in $dir/cabocha_xml/*; do
+  ruby getVerbChunk.rb $f | awk -F, '{print $7}' > $dir/verb_chain/$f:t:r".txt"
 done
