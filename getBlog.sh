cfff6ce2528345610ba3e8e4d253d135e9e6d6ae
Takafumi Sakakibara <takafumi.sakakibara.pdx@gmail.com>
2014-11-09 20:31:31 +0900

    Modify : conditional branch when 404 error

--- getBlog.sh
+++ getBlog.sh
@@ -16,14 +16,18 @@ function my_wget(){
 for url in `cat $bloglist`; do
   output=`echo $url | sed -e 's|^http://ameblo.jp/||' | tr '/' '_'`
   my_wget $url entries/$output
+  wget_status=$? # $? は直前のコマンドの終了ステータス(0が正常)
 
   #wgetが失敗したら、proxyを切り換えて再試行
-  # $? は直前のコマンドの終了ステータス(0が正常)
-  if test $? -ne 0; then
+  if test $wget_status -ne 0; then
+    if test `wget $url --spider 2>&1 | grep "404 Not Found" | wc -l` -gt 0; then
+      #そもそもページが存在しないので、プロキシの問題ではない
+      rm entries/$output
+    fi
+  else
     bad_proxy=`echo $http_proxy | sed -e 's|^http://||' -e 's|/$||'`
     tmp_file=`mktemp`
     grep -v $bad_proxy proxy_list.txt > $tmp_file
-    # echo "#"$bad_proxy >> $tmp_file
     mv $tmp_file proxy_list.txt
 
     my_wget $url entries/$output
