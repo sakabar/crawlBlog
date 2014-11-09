8d191a3722d4bf2e58a15e94d1125e1fe7cb3655
Takafumi Sakakibara <takafumi.sakakibara.pdx@gmail.com>
2014-11-09 21:55:37 +0900

    Modify: don't use extract_text.rb . google_ad_section_start is a key word to extract text

--- extractText.sh
+++ extractText.sh
@@ -7,11 +7,13 @@ fi
 
 
 #<br>などを改行に置換したファイルをmid_data内に置く
+#google_ad_section_start(name=s1 からgoogle_ad_section_end(name=s1 までを抜き出しておく。これが本文
 for f in $dir/entries/*.html; do
-  sed -e 's|<br />|\n|g' -e 's|<br>|\n|g' $f > $dir/mid_data/$f:t
+  output=$dir/mid_data/$f:t
+  sed -e 's|<br />|\n|g' -e 's|<br>|\n|g' $f | awk '/google_ad_section_start\(name=s1/, /google_ad_section_end\(name=s1/ {print $0}' >> $output
 done
 
 for f in $dir/mid_data/*.html; do
-  ruby extract_text.rb $f | nkf -Lu | tr -d '\t' | sed -e 's/ *//g' | grep -v "^$" > $dir/text/$f:t:r".txt"
-  #rm -f $f
+  # ruby extract_text.rb $f | nkf -Lu | tr -d '\t' | sed -e 's/ *//g' | grep -v "^$" > $dir/text/$f:t:r".txt"
+  sed -e 's|<[^>]\+>||g' $f | sed -e 's|</[^>]\+>||g' | nkf -Lu | tr -d '\t' | sed -e 's/ *//g' | grep -v "^$" > $dir/text/$f:t:r".txt"
 done
