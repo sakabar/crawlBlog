0e3a1890cf5536a413ead43fafc093932ed6bf3a
Takafumi Sakakibara <takafumi.sakakibara.pdx@gmail.com>
2014-11-10 22:51:55 +0900

    New: calc PMI fast

---
+++ calc_all_pmi.sh
@@ -0,0 +1,22 @@
+#!/bin/zsh
+#数学関数をインポートする
+zmodload zsh/mathfunc
+
+
+d=$1
+if test -z $d; then
+  d="."
+fi
+
+
+cat $d/verb_chain/*.txt | LC_ALL=C sort | uniq -c | LC_ALL=C sort -nr > $d/counted_all_verbs.txt
+
+cat $d/verb_bigram/*.txt | LC_ALL=C sort | uniq -c | LC_ALL=C sort -nr > $d/counted_all_bigrams.txt
+
+verb_sum=`cat $d/verb_chain/*.txt | wc -l`
+awk -v sum=$verb_sum '{print $1/sum"\t"$2}' $d/counted_all_verbs.txt > $d/all_verbs_probability.txt
+  
+bigram_sum=`cat $d/verb_bigram/*.txt | wc -l`
+awk -v sum=$bigram_sum '{print $1/sum"\t"$2"\t"$3}' $d/counted_all_bigrams.txt > $d/all_bigrams_probability.txt
+
+ruby calc_pmi.rb $d/all_verbs_probability.txt $d/all_bigrams_probability.txt > $d/all_pmi.txt
