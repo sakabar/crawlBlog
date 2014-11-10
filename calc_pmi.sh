9e6dc485262165437e822d72ece833a8448e8875
Takafumi Sakakibara <takafumi.sakakibara.pdx@gmail.com>
2014-11-10 21:39:31 +0900

    New: calc PMI for each file

--- calc_pmi.sh
+++ calc_pmi.sh
@@ -16,9 +16,18 @@ for f in $dir/bigram_probability/*.txt; do
     y=`echo $line | awk -F, '{print $3}'`
     
     #タブはgrepで検索しにくいので,に置換する
-    px=`cat $dir/verb_probability/$f:t | tr '\t' ',' | grep ","\$x"$" | awk -F, '{print $1}'`
-    py=`cat $dir/verb_probability/$f:t | tr '\t' ',' | grep ","\$y"$" | awk -F, '{print $1}'`
-    echo "$line $pxy $px $py $f:t" 1>&2
+    #\\を入れているのは、verbの'*'をメタ文字として解釈させないようにするため
+
+    p1=",\\"$x"$"
+    px=`cat $dir/verb_probability/$f:t | tr '\t' ',' | grep $p1 | awk -F, '{print $1}'`
+
+    p2=",\\"$y"$"
+    py=`cat $dir/verb_probability/$f:t | tr '\t' ',' | grep $p2 | awk -F, '{print $1}'`
+
+    # echo "line = $line" 1>&2
+    # echo "px = $px" 1>&2
+    # echo "py = $py" 1>&2
+    # echo ""
 
     echo $[log10($pxy) - log10($px) - log10($py)]"\t"$x"\t"$y
   done > $dir/bigram_pmi/$f:t
