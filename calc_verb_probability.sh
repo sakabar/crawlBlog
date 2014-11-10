34a6037dcf7b4a6e4ed8b3f82420f213b74ba892
Takafumi Sakakibara <takafumi.sakakibara.pdx@gmail.com>
2014-11-10 18:26:11 +0900

    New: calc verb_probability

---
+++ calc_verb_probability.sh
@@ -0,0 +1,12 @@
+#!/bin/zsh
+
+dir=$1
+if test -z $dir; then
+  dir=`pwd`
+fi
+
+mkdir $dir/verb_probability
+for f in $dir/verb_count/*.txt; do
+  sum=`cat $dir/verb_chain/$f:t | wc -l`
+  awk -v sum=$sum '{print $1/sum"\t"$2}' $f > $dir/verb_probability/$f:t
+done
