4c2378b6cab1dc25aa4289ae87daecfe857593ee
Takafumi Sakakibara <takafumi.sakakibara.pdx@gmail.com>
2014-11-17 22:22:50 +0900

    bugfix: sort PMI file

--- calc_all_pmi.sh
+++ calc_all_pmi.sh
@@ -7,6 +7,9 @@ fi
 
 cat $d/verb_chain/*.txt | LC_ALL=C sort | uniq -c | LC_ALL=C sort -nr > $d/counted_all_verbs.txt
 
+
+#verb_bigramがないといけない
+#あらかじめ、./make_bigram.shをしておくこと
 cat $d/verb_bigram/*.txt | LC_ALL=C sort | uniq -c | LC_ALL=C sort -nr > $d/counted_all_bigrams.txt
 
 verb_sum=`cat $d/verb_chain/*.txt | wc -l`
@@ -15,4 +18,4 @@ awk -v sum=$verb_sum '{print $1/sum"\t"$2}' $d/counted_all_verbs.txt > $d/all_ve
 bigram_sum=`cat $d/verb_bigram/*.txt | wc -l`
 awk -v sum=$bigram_sum '{print $1/sum"\t"$2"\t"$3}' $d/counted_all_bigrams.txt > $d/all_bigrams_probability.txt
 
-ruby calc_pmi.rb $d/all_verbs_probability.txt $d/all_bigrams_probability.txt > $d/all_pmi.txt
+ruby calc_pmi.rb $d/all_verbs_probability.txt $d/all_bigrams_probability.txt | sort -nr > $d/all_pmi.txt
